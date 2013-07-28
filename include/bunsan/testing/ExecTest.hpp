64d821cfce60179b93bb62118e94c8048890f873
Aleksey Filippov <sarum9in@gmail.com>
2013-07-28 21:06:15 +0400

    ExecTest.hpp implementation.

---
+++ include/bunsan/testing/ExecTest.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "bunsan/testing/setenv.hpp"
+
+BUNSAN_TESTING_SETOPT(BOOST_TEST_CATCH_SYSTEM_ERRORS, "no");
---
+++ tests/ExecTest.cpp
@@ -0,0 +1,14 @@
+#define BOOST_TEST_MODULE ExecTest
+#include <boost/test/unit_test.hpp>
+
+#include "bunsan/testing/ExecTest.hpp"
+#include "bunsan/testing/getenv.hpp"
+
+BOOST_AUTO_TEST_SUITE(testing)
+
+BOOST_AUTO_TEST_CASE(nothing_to_do)
+{
+    BOOST_CHECK_EQUAL(bunsan::testing::getenv("BOOST_TEST_CATCH_SYSTEM_ERRORS"), "no");
+}
+
+BOOST_AUTO_TEST_SUITE_END() // testing
