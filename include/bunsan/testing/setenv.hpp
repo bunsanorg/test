e386184576f904d2335fa4282410d7cbb6a5f20f
Aleksey Filippov <sarum9in@gmail.com>
2013-07-28 21:05:02 +0400

    BUNSAN_TESTING_SETOPT() implementation.

--- include/bunsan/testing/setenv.hpp
+++ include/bunsan/testing/setenv.hpp
@@ -5,3 +5,6 @@ namespace bunsan{namespace testing
     int setenv(const char *const key, const char *const value, const bool overwrite);
     int unsetenv(const char *const key);
 }}
+
+#define BUNSAN_TESTING_SETOPT(NAME, VALUE) \
+    const static int NAME##_##__LINE__ = ::bunsan::testing::setenv(#NAME, VALUE, true)
