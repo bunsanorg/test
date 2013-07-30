4ef31d4f4695c9ef39191ac73ee1ed27f617af83
Aleksey Filippov <sarum9in@gmail.com>
2013-07-30 14:03:08 +0400

    filesystem::tempfile implementation.

---
+++ include/bunsan/testing/filesystem/tempfile.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "bunsan/testing/filesystem/tempfiles.hpp"
+
+namespace bunsan{namespace testing{namespace filesystem
+{
+    struct tempfile: private tempfiles
+    {
+        tempfile();
+
+        boost::filesystem::path path;
+    };
+}}}
