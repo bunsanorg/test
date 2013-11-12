2d6984fab0ae159692bb8c83b1e9cb69e68c971c
Aleksey Filippov <sarum9in@gmail.com>
2013-11-12 06:12:35 +0400

    bunsan_testing_project() refactoring.

--- BunsanCmake/testing.cmake
+++ BunsanCmake/testing.cmake
@@ -1,4 +1,18 @@
-macro(bunsan_testing_project)
+macro(bunsan_testing_project_header)
+    bunsan_tests_project_header()
     bunsan_use_bunsan(testing)
-    bunsan_tests_project()
+endmacro()
+
+macro(bunsan_testing_project_aux_add_tests)
+    bunsan_tests_project_aux_add_tests()
+endmacro()
+
+macro(bunsan_testing_project_footer)
+    bunsan_tests_project_footer()
+endmacro()
+
+macro(bunsan_testing_project)
+    bunsan_testing_project_header()
+    bunsan_testing_project_aux_add_tests()
+    bunsan_testing_project_footer()
 endmacro()
