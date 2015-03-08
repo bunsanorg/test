4c31ab6a2bf32f9b307defe4dc19357fe84853aa
Aleksey Filippov <sarum9in@gmail.com>
2015-03-08 23:21:03 +0100

    Fixed exec_test and added not_exec_test.

--- tests/not_exec_test.cpp
+++ tests/not_exec_test.cpp
@@ -1,7 +1,6 @@
-#define BOOST_TEST_MODULE ExecTest
+#define BOOST_TEST_MODULE NotExecTest
 #include <boost/test/unit_test.hpp>
 
-#include <bunsan/testing/exec_test.hpp>
 #include <bunsan/testing/getenv.hpp>
 
 #include <boost/test/unit_test_monitor.hpp>
@@ -10,7 +9,7 @@ BOOST_AUTO_TEST_SUITE(testing)
 
 BOOST_AUTO_TEST_CASE(nothing_to_do)
 {
-    BOOST_CHECK(!boost::unit_test::unit_test_monitor.p_catch_system_errors);
+    BOOST_CHECK(boost::unit_test::unit_test_monitor.p_catch_system_errors);
 }
 
 BOOST_AUTO_TEST_SUITE_END() // testing
