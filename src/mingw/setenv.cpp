3bcde313eaea2bdc3b392ba78ef608a0dfe6eeb5
Aleksey Filippov <sarum9in@gmail.com>
2015-03-09 22:23:23 +0100

    Added MinGW setenv.cpp.

---
+++ src/mingw/setenv.cpp
@@ -0,0 +1,23 @@
+#include <bunsan/testing/setenv.hpp>
+
+#include <string>
+
+#include <cstdlib>
+
+extern "C" int putenv(char *);
+
+namespace bunsan{namespace testing
+{
+    int setenv(const char *const key, const char *const value, const bool overwrite)
+    {
+        if (!overwrite && std::getenv(key))
+            return 0;
+        const std::string data = std::string(key) + "=" + std::string(value);
+        return putenv(const_cast<char *>(data.c_str()));
+    }
+
+    int unsetenv(const char *const key)
+    {
+        return setenv(key, "", true);
+    }
+}}
--- tests/exec_test.cpp
+++ tests/exec_test.cpp
@@ -6,10 +6,13 @@
 
 #include <boost/test/unit_test_monitor.hpp>
 
+#include <cstdlib>
+
 BOOST_AUTO_TEST_SUITE(testing)
 
 BOOST_AUTO_TEST_CASE(nothing_to_do)
 {
+    BOOST_CHECK(std::getenv("BOOST_TEST_CATCH_SYSTEM_ERRORS"));
     BOOST_CHECK(!boost::unit_test::unit_test_monitor.p_catch_system_errors);
 }
 
